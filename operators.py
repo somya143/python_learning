09634efe89f6a42dc752d3076a1d1ca8cea88832
somya143 <maaheersj@gmail.com>
2023-06-23 19:27:00 +0530

    Made a operator file and added some commonly used operators here

---
+++ operators.py
@@ -0,0 +1,8 @@
+# plus operator  (+)
+# minus operator (-)
+# multiply operator(*)
+# divide operator(/)
+# modulo operator(%)
+# power operator(**)
+
+#here multiply operator gets the highest priority
