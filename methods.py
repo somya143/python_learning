7677d94d7a43ac51ea07eadbdbb861b9736f883c
somya143 <maaheersj@gmail.com>
2023-06-23 19:22:16 +0530

    Made a methods file and added some methods to it.

---
+++ methods.py
@@ -0,0 +1,8 @@
+name = "Tony Stark"
+print(name.upper())  #this method will convert all string and characters to uppercase
+print(name.lower()) #this method will convert all string and characters to lowercase
+print(name.find("S")) #this method will return the index of the character or string if or is there else return -1
+print(name.replace("Tony" , "Iron")) #this method will replace the substring or string with the new string or substring
+print("Ironman" in name) #this method will search the particular string or character and if it find it will return true or else it will return false
+
+#all the above methods will not modify the original variable name rather it will create a new variable and update accordingly.
