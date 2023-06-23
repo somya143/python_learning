8fa2e8ada362224c5934d6229e23d5c471b25f0a
somya143 <maaheersj@gmail.com>
2023-06-23 22:50:03 +0530

    made a list file and did some operation on list

---
+++ list.py
@@ -0,0 +1,13 @@
+marks = [92,94,93]
+# marks.insert(2,120) #we can insert an integer in list
+# marks.append(100) #we can append an integer in list end
+
+# for score in marks:
+#     print(score)
+# #print(marks)
+# i=1
+# while i < len(marks):
+#     print(marks[i])
+#     i+=1
+# print(marks[0:2])
+# print(marks[-4])
