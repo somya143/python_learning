1c4b992d7c549995a1fbe4426f2d422615bff52e
somya143 <maaheersj@gmail.com>
2023-06-23 19:14:00 +0530

    Made a type-conversion file and added some type-conversions

---
+++ type-conversion.py
@@ -0,0 +1,5 @@
+oldage = input("Enter your old-age : ")
+newage = int(oldage) + 2  #here we have to convert oldage to number as the compiler takes it as string
+print("This is new age" + " " + str(newage)) #Here newage is a number and compiler cannot cancate a number with string so we have to convert it to string
+print(float(newage))  #this will convert an integer to a decimal or float value
+#bool will convert to bollean value
